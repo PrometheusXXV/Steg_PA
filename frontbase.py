f159d73c4d3fa31debc6c340a854f71dcbcfcf8c
Abhinav <98721835+PrometheusXXV@users.noreply.github.com>
2022-04-16 09:06:19 +0530

    Linked img_in_img with frontbase

--- frontbase.py
+++ frontbase.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
 
-from time import strftime
+#from time import strftime
 from tkinter import ttk
 from tkinter import filedialog
 from tkinter.filedialog import *
-import os
+#import os
 import PIL.Image
 from PIL import ImageTk
 from tkinter import *
 
-from LSB import *  ########to import other files at module level make every thing as function but not as main otherwise main will run and cause problem
+##### importing the scripts of encrpytion and decryption modules
+from LSB import *  
 from PVD import *
 from Audio_LSB import *
 from custom_audio_enc import *
 from phase_coding import *
 from prng_2 import*
+from img_in_img import *
 
 #path = ''
 
@@ -44,43 +46,10 @@ def show_browse_file(path,image_frame):
 def clear_frame(frame):
       frame.destroy()
 
-"""def menu_bar():
-    
-    menubar = Menu(root)
-    #file menu
-    file = Menu(menubar,tearoff=0)
-    menubar.add_cascade(label="File" , menu=file)
-
-    global current_frame
-    file.add_command(label ='Home', command =None)
-    file.add_separator()
-    file.add_command(label ='Exit', command = root.destroy)
-
-    #images steg menu
-    images = Menu(menubar, tearoff=0)
-    menubar.add_cascade(label="Images" , menu=images)
-    images.add_command(label ='LSB', command = lambda:[frame_main.destroy()])
-    #images.add_command(label ='Masking and Filtering', command = None)
-    images.add_command(label ='PVD', command = None)
-    images.add_command(label ='PRNG', command = None)
-    #images.add_command(label ='AES Encryption', command = None)
-
-    #audio steg menu
-    audio = Menu(menubar, tearoff=0)
-    menubar.add_cascade(label="Audio" , menu=audio)
-    audio.add_command(label ='LSB', command = lambda:[current_frame.destroy(),LSB_Audio()])
-    audio.add_command(label ='Custom', command = None)
-    audio.add_command(label ='Phase Coding', command = None)
-    #audio.add_command(label ='AES Encryption', command = None)
-
-    root.config(menu=menubar)  #display menu
- """   
-
 def win_LSB():
     root.title("STEG_PA - Image Steganography - LSB")
     frame_LSB = Frame(root)
     frame_LSB.pack(side="top", expand=True, fill="both")
-    current_frame =frame_LSB
     path =''
     browse = Button(frame_LSB,text = "Browse",bd ='2', command = (lambda:[open_file(image_frame),label_used.destroy()])) ######### problem not able to store file path
     browse.config(font=("Courier",12))
@@ -113,29 +82,10 @@ def win_LSB():
     label.image =img
     label.pack()
     
-"""
-def win_LSB():
-    root.title("STEG_PA - Image Steganography - LSB")
-    frame_LSB = Frame(root)
-    frame_LSB.pack(side="top", expand=True, fill="both")
-    
-    lsb_en = Button(frame_LSB,text = "Encode",bd ='2', command = (lambda:[])) ######### problem not able to store file path
-    lsb_en.config(font=("Courier",11))
-    lsb_en.place(relx=0.5,rely=0.1,anchor=CENTER)
-
-    lsb_de = Button(frame_LSB,text = "Decode",bd ='2', command = (lambda:[open_file()])) ######### problem not able to store file path
-    lsb_de.config(font=("Courier",11))
-    lsb_de.place(relx=0.5,rely=0.3,anchor=CENTER)
-
-    back = Button(frame_LSB,text = "BACK",bd='2',command=lambda:[frame_LSB.destroy(),Image_steg()])
-    back.config(font=("Courier",16))
-    back.place(relx=0.5,rely=0.8,anchor=CENTER)
-"""
 def main_win():
     root.title("STEG_PA")
     global frame_main
     frame_main = Frame(root)
-    current_frame =frame_main
     
     Images = Button(frame_main,text = "Image Steganography",bd='4',command=lambda:[frame_main.destroy(),Image_steg()])
     Images.config(font=("Courier",16))
@@ -190,7 +140,7 @@ def image_open(frame):
         file = path.split('\\');
         file_name = Label(frame,text = file[-1])
         file_name.config(font = ("Times New Roman",13))
-        file_name.place(relx = 0.2,rely = 0.55,anchor = CENTER)
+        file_name.place(relx = 0.2,rely = 0.15,anchor = CENTER)
         
     else:
         path_output = Label(frame,text = "Image File \nNot Selected")
@@ -199,14 +149,62 @@ def image_open(frame):
 
         file_name = Label(frame,text ="                              ")
         file_name.config(font = ("Times New Roman",13))
-        file_name.place(relx = 0.2,rely = 0.55,anchor = CENTER)
+        file_name.place(relx = 0.2,rely = 0.15,anchor = CENTER)
+
+def image_open_2(frame):
+    global path_2
+    file = filedialog.askopenfile(mode='r', filetypes =[('Files','*.png')]) #'Files|*.jpg;*.jpeg;*.png;'
+    if file:
+        path_2 = (os.path.abspath(file.name))          ### to get path using tkinter browse button
+        file.close()
+    else :
+        path_2 =''
+    print(path_2)
+    if(len(path_2) != 0):
+        path_output = Label(frame,text = "Cover Image Selected")
+        path_output.config(font = ("magneto",13))
+        path_output.place(relx = 0.5,rely = 0.12,anchor = CENTER)
+
+        x = PIL.Image.open(path_2)
+        wd,ht = x.size
+        if(wd>ht):
+            factor = 200/wd
+            wd = 200
+            ht = int(ht*factor)
+        elif(ht>wd):
+            factor = 200/wd
+            ht = 200
+            wd = int(wd*factor)
+        else:
+            wd = 200
+            ht = 200
+        x = x.resize((wd,ht))
+        img = ImageTk.PhotoImage(x)
+        label =Label(frame, image=img)
+        
+        label.image =img
+        label.place(anchor=CENTER,relx =0.5,rely =0.33)
+        #label.pack()
+
+        file = path_2.split('\\');
+        file_name = Label(frame,text = file[-1])
+        file_name.config(font = ("Times New Roman",13))
+        file_name.place(relx = 0.5,rely = 0.15,anchor = CENTER)
+        
+    else:
+        path_output = Label(frame,text = "Cover Image \nNot Selected")
+        path_output.config(font = ("Magneto",13))
+        path_output.place(relx = 0.5,rely = 0.12,anchor = CENTER)
+
+        file_name = Label(frame,text ="                              ")
+        file_name.config(font = ("Times New Roman",13))
+        file_name.place(relx = 0.5,rely = 0.55,anchor = CENTER)
 
     
 def image_pvd():
     root.title("STEG_PA - Image Steganography - PVD")
 
     frame_image_pvd = Frame(root)
-    current_frame =frame_image_pvd
 
     browse = Button(frame_image_pvd,text = "Browse",command = lambda:[(image_open(frame_image_pvd))])
     browse.config(font = ("Courier",12))
@@ -248,7 +246,6 @@ def image_prng():
     root.title("STEG_PA - Image Steganography - PRNG")
 
     frame_image_prng = Frame(root)
-    current_frame =frame_image_prng
 
     browse = Button(frame_image_prng,text = "Browse",command = lambda:[(image_open(frame_image_prng))])
     browse.config(font = ("Courier",12))
@@ -305,7 +302,6 @@ def image_LSB():
     root.title("STEG_PA - Image Steganography - LSB")
 
     frame_image_lsb = Frame(root)
-    current_frame =frame_image_lsb
 
     browse = Button(frame_image_lsb,text = "Browse",command = lambda:[(image_open(frame_image_lsb))])
     browse.config(font = ("Courier",12))
@@ -342,20 +338,57 @@ def image_LSB():
     L2.place(relx =0.5, rely =0.42 ,anchor= CENTER )
 
     frame_image_lsb.pack(side= "top",expand =True,fill = 'both')
+
+def image_IH():
+    root.title("STEG_PA - Image Steganography - Image Hiding")
+
+    frame_image_ih = Frame(root)
+
+    hide = Button(frame_image_ih,text = "To Hide",command = lambda:[(image_open(frame_image_ih))])
+    hide.config(font = ("Courier",12))
+    hide.place(relx = 0.26 ,rely = 0.7,anchor = CENTER)
+
+    cover = Button(frame_image_ih,text = "Cover",command = lambda:[(image_open_2(frame_image_ih))])
+    cover.config(font = ("Courier",12))
+    cover.place(relx = 0.42 ,rely = 0.7,anchor = CENTER)
+    
+    encode = Button(frame_image_ih,text = "Encode",command = lambda:[ih_encode(path,path_2,save_path,frame_image_ih )])
+    encode.config(font = ("Courier",12))
+    encode.place(relx = 0.58 ,rely = 0.7,anchor = CENTER)
+    
+    decode = Button(frame_image_ih,text = "Decode",command = lambda:[ih_decode(path_2,frame_image_ih)])
+    decode.config(font = ("Courier",12))
+    decode.place(relx = 0.74 ,rely = 0.7,anchor = CENTER)
+
+    reset = Button(frame_image_ih,text = "RESET",command = lambda:[clear_frame(frame_image_ih),image_IH()])
+    reset.config(font = ("Courier",12))
+    reset.place(relx = 0.5 ,rely = 0.8,anchor = CENTER)
+
+    back =Button(frame_image_ih,text = "Back", command =lambda:[frame_image_ih.destroy(),Image_steg()])
+    back.config(font = ("Courier",16))
+    back.place(relx = 0.5 , rely =0.9, anchor = CENTER)
+
+    save_path =Text(frame_image_ih,height =2,width =20)
+    save_path.place(relx = 0.5 ,rely= 0.60, anchor =CENTER)
+
+    L2 = Label(frame_image_ih,text = "Enter File Name \nWith Extension to SAVE \nthe Encoded File")
+    L2.config(font=("Magneto",11))
+    L2.place(relx =0.5, rely =0.52 ,anchor= CENTER )
+
+    frame_image_ih.pack(side= "top",expand =True,fill = 'both')
     
 def Image_steg():
     root.title("STEG_PA - Image Steganography")
        
     frame_image= Frame(root)
-    current_frame =frame_image
     LSB = Button(frame_image,text = "LSB",bd='4',command=lambda:[frame_image.destroy(),image_LSB()])
     LSB.config(font=("Courier",16))
     LSB.place(relx=0.5,rely=0.2,anchor=CENTER)
 
-    """mask = Button(frame_image,text = "MASKING AND FILTERING",bd='4',command=None)
-    mask.config(font=("Courier",16))
-    mask.place(relx=0.5,rely=0.65,anchor=CENTER)
-    """
+    IH = Button(frame_image,text = "IMAGE HIDING",bd='4',command=lambda:[frame_image.destroy(),image_IH()])
+    IH.config(font=("Courier",16))
+    IH.place(relx=0.5,rely=0.65,anchor=CENTER)
+    
     PVD = Button(frame_image,text = "PVD",bd='4',command=lambda:[frame_image.destroy(),image_LSB()])
     PVD.config(font=("Courier",16))
     PVD.place(relx=0.5,rely=0.35,anchor=CENTER)
@@ -407,8 +440,7 @@ def LSB_Audio():
     root.title("STEG_PA - Audio Steganography - LSB")
 
     frame_audio_lsb = Frame(root)
-    current_frame =frame_audio_lsb
-
+    
     browse = Button(frame_audio_lsb,text = "Browse",command = lambda:[(audio_open(frame_audio_lsb))])
     browse.config(font = ("Courier",12))
     browse.place(relx = 0.35 ,rely = 0.7,anchor = CENTER)
@@ -452,7 +484,6 @@ def custom_audio():
     root.title("STEG_PA - Audio Steganography - CUSTOM")
 
     frame_audio_custom = Frame(root)
-    current_frame =frame_audio_custom
 
     browse = Button(frame_audio_custom,text = "Browse",command = lambda:[(audio_open(frame_audio_custom))])
     browse.config(font = ("Courier",12))
@@ -501,7 +532,6 @@ def phase_coding_audio():
     root.title("STEG_PA - Audio Steganography - PHASE CODING")
 
     frame_audio_pc = Frame(root)
-    current_frame =frame_audio_pc
 
     browse = Button(frame_audio_pc,text = "Browse",command = lambda:[(audio_open(frame_audio_pc))])
     browse.config(font = ("Courier",12))
@@ -545,7 +575,6 @@ def Audio_steg():######## root change karna hai
     root.title("STEG_PA - Audio Steganography")
        
     frame_audio= Frame(root)
-    current_frame =frame_audio
     
     LSB = Button(root,text = "LSB",bd='4',command=lambda:[frame_audio.destroy(),LSB_Audio()])
     LSB.config(font=("Courier",16))
@@ -574,13 +603,13 @@ root.geometry("800x700")
 #LSB_Audio()
 #main_win()
 #Image_steg()
+image_IH()
 #Audio_steg()
 #phase_coding_audio()
 #image_LSB()
 #image_pvd()
-image_prng()
+#image_prng()
 #win_LSB()
 #custom_audio()
 root.mainloop()
 
-#  from LSB import *  ########to import other files at module level make every thing as function but not as main otherwise main will run and cause problem
